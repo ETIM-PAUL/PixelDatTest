f0e97fd7bdd15a2b29f2f3bc9a58c8cddd5df30a
etim <etimpaul22@gmail.com>
2024-03-21 17:56:47 +0100

    added UI for transfer

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,11 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
 import TopNav from './components/TopNav';
+import { useState } from 'react';
 
 function App() {
+  const [disabled, setDisabled] = useState(false)
+  const [isLoading, setLoading] = useState(false)
+
   return (
     <div className="App">
       <TopNav />
+
+      <div className='border-gray-200 bg-gray-900 w-full md:max-w-xl mx-auto mt-20 py-10'>
+        <div className='px-4 pb-4'>
+          <span className='text-white text-lg md:text-2xl text-center block w-full font-bold'>Transfer BUSD</span>
+        </div>
+
+        <div className='pt-4 max-w-sm mx-auto'>
+          <div className='inline-grid w-full'>
+            <label className='text-white font-medium text-lg pb-1'>Ethereum Address</label>
+            <input className='h-10 w-full py-1 px-3 border rounded-md focus:outline-none' placeholder='Enter ethereum address' />
+          </div>
+          <div className='inline-grid w-full mt-4'>
+            <label className='text-white font-medium text-lg pb-1'>BUSD Amount</label>
+            <input className='h-10 w-full py-1 px-3 border rounded-md focus:outline-none' placeholder='Enter BUSD amount' />
+          </div>
+        </div>
+
+        <div className='max-w-sm mx-auto mt-12'>
+          <button disabled={disabled} className={`bg-blue-500 w-full h-12 text-white font-bold text-xl rounded-md disabled:cursor-not-allowed disabled bg-blue-800 disabled:opacity-50`}>{isLoading ? "Processing" : "Transfer BUSD"}</button>
+        </div>
+      </div>
     </div>
   );
 }
