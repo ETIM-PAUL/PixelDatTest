ae68ea806b9f3f89e566d476ef2359a7f49a1802
etim <etimpaul22@gmail.com>
2024-06-07 20:25:29 +0100

    link

--- Frontend/src/components/TopNav.jsx
+++ Frontend/src/components/TopNav.jsx
@@ -5,10 +5,10 @@ function TopNav() {
   return (
     <nav className="bg-white border-gray-200 dark:bg-gray-900">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+        <Link to="/another" className="flex items-center space-x-3 rtl:space-x-reverse">
           <img src={logo} className="h-12" alt="BushPortal Logo" />
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">BUSDPortal</span>
-        </a>
+        </Link>
         <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
           <Link to="/another">
             <button className="text-white font-medium rounded-lg text-sm px-4 py-3 text-center">
